b21b2b33c9fa47bb60b271cfd34a7339e6a8df80
Humberto <humberto2do@gmail.com>
2025-03-28 12:24:07 -0600

    create section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,18 @@ import { HeaderComponent } from './header/header.component';
 import { ChartComponent } from './chart/chart.component';
 import { BalanceCardComponent } from './balance-card/balance-card.component';
 import { ComparisonBarsComponent } from './comparison-bars/comparison-bars.component';
+import { SectionComponent } from './section/section.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TableComponent, HeaderComponent,ChartComponent, BalanceCardComponent, ComparisonBarsComponent],
+  imports: [
+    RouterOutlet,
+    TableComponent,
+    HeaderComponent,
+    ChartComponent,
+    BalanceCardComponent,
+    ComparisonBarsComponent,
+    SectionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/section/section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'budget-section',
+  imports: [],
+  templateUrl: './section.component.html',
+  styleUrl: './section.component.css'
+})
+export class SectionComponent {
+
+}
