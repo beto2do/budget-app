5672763533c766a5f2c848229025fe443f047bdb
Humberto <humberto2do@gmail.com>
2025-04-29 16:28:21 -0600

    Add lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,26 @@
 import { Routes } from '@angular/router';
-import { SummaryComponent } from './components/summary/summary.component';
-import { TransactionsComponent } from './components/transactions/transactions.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: 'summary', component: SummaryComponent },
-  { path: 'transactions', component: TransactionsComponent },
+  {
+    path: 'summary',
+    loadComponent: () =>
+      import('./components/summary/summary.component').then(
+        (m) => m.SummaryComponent,
+      ),
+  },
+  {
+    path: 'transactions',
+    loadComponent: () =>
+      import('./components/transactions/transactions.component').then(
+        (m) => m.TransactionsComponent,
+      ),
+  },
   { path: '', redirectTo: '/summary', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/page-not-found/page-not-found.component').then(
+        (m) => m.PageNotFoundComponent,
+      ),
+  },
 ];
