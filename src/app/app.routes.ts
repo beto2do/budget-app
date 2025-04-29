3def1add153ed3af62d0dd2778fbe95688357af5
Humberto <humberto2do@gmail.com>
2025-04-29 15:07:12 -0600

    Add wildcard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { SummaryComponent } from './components/summary/summary.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/summary', pathMatch: 'full' },
   { path: 'summary', component: SummaryComponent },
   { path: 'transactions', component: TransactionsComponent },
+  { path: '', redirectTo: '/summary', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
---
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'budget-page-not-found',
+  imports: [],
+  templateUrl: './page-not-found.component.html',
+})
+export class PageNotFoundComponent {}
