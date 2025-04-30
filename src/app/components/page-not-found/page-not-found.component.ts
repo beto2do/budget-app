c1e5c21f692274ff7ca0a6b32ec0cf374d326ff8
Humberto <humberto2do@gmail.com>
2025-04-29 20:56:08 -0600

    Add content in 404 page

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'budget-page-not-found',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './page-not-found.component.html',
 })
 export class PageNotFoundComponent {}
