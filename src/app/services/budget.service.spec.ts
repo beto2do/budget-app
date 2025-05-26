a4f1c6184cc61b247a7ea71e09f17919899b063d
Humberto <humberto2do@gmail.com>
2025-05-25 18:13:19 -0600

    unit test services

--- src/app/services/budget.service.spec.ts
+++ src/app/services/budget.service.spec.ts
@@ -13,4 +13,18 @@ describe('BudgetService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a list of products', () => {
+    const products = service.getProducts();
+    expect(products.length).toBe(5);
+    expect(products[0].name).toBe('Internet');
+  });
+
+  it('should fetch products asynchronously', (done) => {
+    service.fetchProducts().then((products) => {
+      expect(products.length).toBe(5);
+      expect(products[1].name).toBe('Transportation');
+      done();
+    });
+  });
 });
--- src/app/services/form-controller.service.spec.ts
+++ src/app/services/form-controller.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FormControllerService } from './form-controller.service';
+import { ControlBase } from '../models/form-base';
 
 describe('FormControllerService', () => {
   let service: FormControllerService;
@@ -12,5 +13,25 @@ describe('FormControllerService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create a form group from controllers', () => {
+    const controllers: ControlBase<string>[] = [
+      new ControlBase({ key: 'name', required: true }),
+      new ControlBase({ key: 'age', required: false }),
+    ];
+    const formGroup = service.toFormGroup(controllers);
+    expect(formGroup.controls['name'].valid).toBeFalse();
+    expect(formGroup.controls['name'].value).toEqual('');
+    expect(formGroup.controls['age'].valid).toBeTrue();
+    expect(formGroup.controls['age'].value).toEqual('');
+  });
+
+  it('should return income controllers', () => {
+    service.getIncomeControllers().subscribe((controllers) => {
+      expect(controllers.length).toBeGreaterThan(0);
+      expect(controllers[0].key).toEqual('product-income');
+      expect(controllers[1].key).toEqual('amount-income');
+    });
   });
 });
