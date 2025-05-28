8708227a1bc62372c2ba1539e0d8717663d95f94
Humberto <humberto2do@gmail.com>
2025-05-28 16:19:57 -0600

    unit test budget table

--- src/app/components/table/table.component.spec.ts
+++ src/app/components/table/table.component.spec.ts
@@ -12,11 +12,25 @@ describe('TableComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(TableComponent);
+    fixture.componentRef.setInput('tableTitle', 'Test Table');
+    fixture.componentRef.setInput('products', [
+      { id: 1, name: 'Product 1', estimate: 100 },
+    ]);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the correct table title', () => {
+    expect(component.tableTitle()).toBe('Test Table');
+  });
+
+  it('should have products defined', () => {
+    expect(component.products()).toEqual([
+      { id: 1, name: 'Product 1', estimate: 100 },
+    ]);
   });
 });
