55aab7aa498f7d5c638e72daca3c0c345e8eef55
Humberto <humberto2do@gmail.com>
2025-05-28 11:58:34 -0600

    unit test header component

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -10,7 +10,6 @@ describe('HeaderComponent', () => {
     await TestBed.configureTestingModule({
       imports: [HeaderComponent],
     }).compileComponents();
-
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,5 +17,16 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update initialBalanceChanged on changeBalance', () => {
+    const hostElement: HTMLElement = fixture.nativeElement;
+    const nameInput: HTMLInputElement = hostElement.querySelector('input')!;
+    spyOn(component, 'changeBalance').and.callThrough();
+    nameInput.value = '1000';
+    nameInput.dispatchEvent(new Event('input'));
+
+    expect(component.changeBalance).toHaveBeenCalled();
+    expect(component.initialBalanceChanged()).toBe(1000);
   });
 });
