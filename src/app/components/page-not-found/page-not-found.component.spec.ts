536cadaf72bdb35048e72d62b7758c1e7d8acd91
Humberto <humberto2do@gmail.com>
2025-05-28 17:31:51 -0600

    fix tests

--- src/app/components/page-not-found/page-not-found.component.spec.ts
+++ src/app/components/page-not-found/page-not-found.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 describe('PageNotFoundComponent', () => {
@@ -8,7 +8,7 @@ describe('PageNotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageNotFoundComponent],
+      imports: [RouterModule.forRoot([]), PageNotFoundComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PageNotFoundComponent);
