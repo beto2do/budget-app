1eacee5cea6da12096716ae74d1616c3dcc0df39
Humberto <humberto2do@gmail.com>
2025-05-28 21:24:59 -0600

    fix test

--- src/app/directives/orange-color.directive.spec.ts
+++ src/app/directives/orange-color.directive.spec.ts
@@ -1,8 +1,27 @@
+import { Component, DebugElement } from '@angular/core';
 import { OrangeColorDirective } from './orange-color.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: ` <h1 budgetOrangeColor>Test Budget</h1>`,
+  imports: [OrangeColorDirective],
+})
+class TestColorComponent {}
 
 describe('OrangeColorDirective', () => {
+  let fixture: ComponentFixture<TestColorComponent>;
+  let directive: DebugElement;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      imports: [OrangeColorDirective],
+    }).createComponent(TestColorComponent);
+    fixture.detectChanges();
+    directive = fixture.debugElement.query(By.directive(OrangeColorDirective));
+  });
+
   it('should create an instance', () => {
-    const directive = new OrangeColorDirective();
     expect(directive).toBeTruthy();
   });
 });
